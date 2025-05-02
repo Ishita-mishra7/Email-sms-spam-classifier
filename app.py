dfefe6ce41c9a8bf1a1b33d27732951b46261687
Ishita mishra <ishitamishra076@gmail.com>
2025-05-02 12:27:54 +0530

    Fixed nltk download issue

--- app.py
+++ app.py
@@ -5,15 +5,22 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 
-# Download necessary NLTK resources
-nltk.download('punkt')
-nltk.download('stopwords')
+# Ensure necessary NLTK resources are downloaded
+try:
+    nltk.data.find('tokenizers/punkt')
+except LookupError:
+    nltk.download('punkt')
+
+try:
+    nltk.data.find('corpora/stopwords')
+except LookupError:
+    nltk.download('stopwords')
 
 ps = PorterStemmer()
 
 def transform_text(text):
     text = text.lower()
-    text = nltk.word_tokenize(text)
+    text = nltk.word_tokenize(text)  # This should work after downloading 'punkt'
     y = []
     for i in text:
         if i.isalnum():
@@ -31,7 +38,7 @@ def transform_text(text):
         y.append(ps.stem(i))
     return " ".join(y)
 
-# Load vectorizer and model
+# Load the vectorizer and model
 tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
 model = pickle.load(open('model.pkl', 'rb'))
 
@@ -40,9 +47,13 @@ st.title("Email/SMS Spam Classifier")
 input_sms = st.text_area("Enter the message")
 
 if st.button("Predict"):
+    # Preprocess the input
     transformed_sms = transform_text(input_sms)
+    # Vectorize the input
     vector_input = tfidf.transform([transformed_sms])
+    # Predict the result
     result = model.predict(vector_input)[0]
+    # Display the result
     if result == 1:
         st.header("Spam")
     else:
